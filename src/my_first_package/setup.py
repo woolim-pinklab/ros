cdfc27c7c6a55d02c7a0d76966e463327c709b26
Woolim Park <woolim@pinklab.art>
2026-07-17 22:56:21 +0900

    01.my_first_package, my_first_node

---
+++ src/my_first_package/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_first_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pinklab',
+    maintainer_email='woolim@pinklab.art',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'my_first_node = my_first_package.my_first_node:main'
+        ],
+    },
+)
