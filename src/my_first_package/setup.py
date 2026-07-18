9a0db8cae3a82471211890e426588b867e6fa6eb
Woolim Park <woolim@pinklab.art>
2026-07-18 11:44:16 +0900

    05.turtle_cmd_and_pose

--- src/my_first_package/setup.py
+++ src/my_first_package/setup.py
@@ -26,7 +26,8 @@ setup(
         'console_scripts': [
             'my_first_node = my_first_package.my_first_node:main',
             'my_subscriber = my_first_package.my_subscriber:main',
-            'my_publisher = my_first_package.my_publisher:main'
+            'my_publisher = my_first_package.my_publisher:main',
+            'turtle_cmd_and_pose = my_first_package.turtle_cmd_and_pose:main'
         ],
     },
 )
