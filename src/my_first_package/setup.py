5c2dba1a1e86df261c916abafe9f0b4e93e1f93d
Woolim Park <woolim@pinklab.art>
2026-07-18 20:06:39 +0900

    13.my_multi_thread

--- src/my_first_package/setup.py
+++ src/my_first_package/setup.py
@@ -29,7 +29,8 @@ setup(
             'my_publisher = my_first_package.my_publisher:main',
             'turtle_cmd_and_pose = my_first_package.turtle_cmd_and_pose:main',
             "my_service_server = my_first_package.my_service_server:main",
-            "dist_turtle_action_server = my_first_package.dist_turtle_action_server:main"
+            "dist_turtle_action_server = my_first_package.dist_turtle_action_server:main",
+            "my_multi_thread = my_first_package.my_multi_thread:main"
         ],
     },
 )
