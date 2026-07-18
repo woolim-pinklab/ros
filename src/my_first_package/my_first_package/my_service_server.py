80626455ef216550e0d468e59388fb82b8a03cd2
Woolim Park <woolim@pinklab.art>
2026-07-18 16:00:35 +0900

    09.my_service_server v3

--- src/my_first_package/my_first_package/my_service_server.py
+++ src/my_first_package/my_first_package/my_service_server.py
@@ -1,7 +1,10 @@
 from my_first_package_msgs.srv import MultiSpawn 
 from turtlesim.srv import TeleportAbsolute
+from turtlesim.srv import Spawn
 
+import time
 import rclpy as rp
+import numpy as np
 from rclpy.node import Node
 
 
@@ -9,20 +12,37 @@ class MultiSpawning(Node):
 
     def __init__(self):
         super().__init__('multi_spawn')
-        self.server = self.create_service(MultiSpawn, 'multi_spawn', self.callback_service)
+        self.server = self.create_service(MultiSpawn, 'multi_spawn', self.callback_service)  
         self.teleport = self.create_client(TeleportAbsolute, '/turtle1/teleport_absolute')
+        self.spawn = self.create_client(Spawn, '/spawn')
         self.req_teleport = TeleportAbsolute.Request()
+        self.req_spawn = Spawn.Request()
+        self.center_x = 5.54
+        self.center_y = 5.54
 
+
+    def calc_position(self, n, r):
+        gap_theta = 2*np.pi / n
+        theta = [gap_theta*n for n in range(n)]
+        x = [r*np.cos(th) for th in theta]
+        y = [r*np.sin(th) for th in theta]
+    
+        return x, y, theta
 
     def callback_service(self, request, response):
-        # print("Request : ", request)
+        x, y, theta = self.calc_position(request.num, 3)
 
-        # response.x = [1.0, 2.0, 3.0]
-        # response.y = [10.0, 20.0]
-        # response.theta = [100.0, 200.0, 300.0]
-        self.req_teleport.x = 1.0
-        self.teleport.call_async(self.req_teleport)
-        
+        for n in range(len(theta)):
+            self.req_spawn.x = x[n] + self.center_x
+            self.req_spawn.y = y[n] + self.center_y
+            self.req_spawn.theta = theta[n]
+            self.spawn.call_async(self.req_spawn)
+            time.sleep(0.5)
+
+        response.x = x
+        response.y = y 
+        response.theta = theta 
+
         return response
 
 def main(args=None):
