e1447e0863753710eb84114a2488eca1d0b9eb09
Woolim Park <woolim@pinklab.art>
2026-07-18 20:00:24 +0900

    12.dist_turtle_action_server v2

--- src/my_first_package/my_first_package/dist_turtle_action_server.py
+++ src/my_first_package/my_first_package/dist_turtle_action_server.py
@@ -1,6 +1,7 @@
 import rclpy as rp 
 from rclpy.action import ActionServer
 from rclpy.node import Node
+import time
 
 from my_first_package_msgs.action import DistTurtle
 
@@ -11,6 +12,11 @@ class DistTurtleServer(Node):
         self.action_server = ActionServer(self, DistTurtle, 'dist_turtle', self.execute_callback)
 
     def execute_callback(self, goal_handle):
+        feedback_msg = DistTurtle.Feedback()
+        for n in range(0, 10):
+            feedback_msg.remained_dist = float(n)
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(0.5)
         goal_handle.succeed()
         result = DistTurtle.Result()
         return result
