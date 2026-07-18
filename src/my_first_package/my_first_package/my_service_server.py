ddee2fd3d1f3efe8da71d20dc3404b515f4e6ff0
Woolim Park <woolim@pinklab.art>
2026-07-18 14:33:13 +0900

    08.my_service_server v2

--- src/my_first_package/my_first_package/my_service_server.py
+++ src/my_first_package/my_first_package/my_service_server.py
@@ -1,4 +1,5 @@
 from my_first_package_msgs.srv import MultiSpawn 
+from turtlesim.srv import TeleportAbsolute
 
 import rclpy as rp
 from rclpy.node import Node
@@ -8,15 +9,20 @@ class MultiSpawning(Node):
 
     def __init__(self):
         super().__init__('multi_spawn')
-        self.server = self.create_service(MultiSpawn, 'multi_spawn', self.callback_service)  
+        self.server = self.create_service(MultiSpawn, 'multi_spawn', self.callback_service)
+        self.teleport = self.create_client(TeleportAbsolute, '/turtle1/teleport_absolute')
+        self.req_teleport = TeleportAbsolute.Request()
+
 
     def callback_service(self, request, response):
-        print("Request : ", request)
+        # print("Request : ", request)
 
-        response.x = [1.0, 2.0, 3.0]
-        response.y = [10.0, 20.0]
-        response.theta = [100.0, 200.0, 300.0]
-
+        # response.x = [1.0, 2.0, 3.0]
+        # response.y = [10.0, 20.0]
+        # response.theta = [100.0, 200.0, 300.0]
+        self.req_teleport.x = 1.0
+        self.teleport.call_async(self.req_teleport)
+        
         return response
 
 def main(args=None):
